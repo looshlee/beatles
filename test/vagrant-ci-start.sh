d72e9f94366b77126ecf647172828b15dc78050c
elle c <14810964+looshlee@users.noreply.github.com>
2022-07-27 10:31:20 -0700

    Initialize multi-stack-dev-vm

---
+++ test/vagrant-ci-start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "destroying vms in case this is a retry"
+vagrant destroy k8s1-${K8S_VERSION} k8s2-${K8S_VERSION} --force
+
+echo "starting vms"
+vagrant up k8s1-${K8S_VERSION} k8s2-${K8S_VERSION} --provision
+
+echo "getting vagrant kubeconfig from provisioned vagrant cluster"
+./get-vagrant-kubeconfig.sh > vagrant-kubeconfig
+
+echo "checking whether kubeconfig works for vagrant cluster"
+kubectl get nodes
+
+echo "adding local docker registry to cluster"
+helm template k8sT/manifests/registry-adder --set IP="$(./print-node-ip.sh)" > registry-adder.yaml
+kubectl apply -f registry-adder.yaml
+
+echo "labeling nodes"
+kubectl label node k8s1 cilium.io/ci-node=k8s1
+kubectl label node k8s2 cilium.io/ci-node=k8s2
