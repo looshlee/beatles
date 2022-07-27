61a9c059b1b3b8e1b5c5e140c742d54d48817e3c
elle c <14810964+looshlee@users.noreply.github.com>
2022-07-27 10:19:39 -0700

    Initialize wip-sockmap-v4

---
+++ pkg/endpointmanager/manager.go
@@ -0,0 +1,378 @@
+// Copyright 2016-2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package endpointmanager
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/cilium/cilium/pkg/endpoint"
+	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/policy"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "endpoint-manager")
+
+	// mutex protects endpoints and endpointsAux
+	mutex lock.RWMutex
+
+	// endpoints is the global list of endpoints indexed by ID. mutex must
+	// be held to read and write.
+	endpoints    = map[uint16]*endpoint.Endpoint{}
+	endpointsAux = map[string]*endpoint.Endpoint{}
+)
+
+func init() {
+	// EndpointCount is a function used to collect this metric. We cannot
+	// increment/decrement a gauge since we invoke Remove gratuitiously and that
+	// would result in negative counts.
+	// It must be thread-safe.
+	metrics.EndpointCount = prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Namespace: metrics.Namespace,
+		Name:      "endpoint_count",
+		Help:      "Number of endpoints managed by this agent",
+	},
+		func() float64 { return float64(len(GetEndpoints())) },
+	)
+	metrics.MustRegister(metrics.EndpointCount)
+}
+
+// Insert inserts the endpoint into the global maps.
+func Insert(ep *endpoint.Endpoint) {
+	// No need to check liveness as an endpoint can only be deleted via the
+	// API after it has been inserted into the manager.
+	ep.UnconditionalRLock()
+	mutex.Lock()
+
+	endpoints[ep.ID] = ep
+	updateReferences(ep)
+
+	mutex.Unlock()
+	ep.RUnlock()
+
+	ep.RunK8sCiliumEndpointSync()
+}
+
+// Lookup looks up the endpoint by prefix id
+func Lookup(id string) (*endpoint.Endpoint, error) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+
+	prefix, eid, err := endpointid.ParseID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	switch prefix {
+	case endpointid.CiliumLocalIdPrefix:
+		n, err := endpointid.ParseCiliumID(id)
+		if err != nil {
+			return nil, err
+		}
+		return lookupCiliumID(uint16(n)), nil
+
+	case endpointid.CiliumGlobalIdPrefix:
+		return nil, fmt.Errorf("Unsupported id format for now")
+
+	case endpointid.ContainerIdPrefix:
+		return lookupDockerID(eid), nil
+
+	case endpointid.DockerEndpointPrefix:
+		return lookupDockerEndpoint(eid), nil
+
+	case endpointid.ContainerNamePrefix:
+		return lookupDockerContainerName(eid), nil
+
+	case endpointid.PodNamePrefix:
+		return lookupPodNameLocked(eid), nil
+
+	case endpointid.IPv4Prefix:
+		return lookupIPv4(eid), nil
+
+	default:
+		return nil, fmt.Errorf("Unknown endpoint prefix %s", prefix)
+	}
+}
+
+// LookupCiliumID looks up endpoint by endpoint ID
+func LookupCiliumID(id uint16) *endpoint.Endpoint {
+	mutex.RLock()
+	ep := lookupCiliumID(id)
+	mutex.RUnlock()
+	return ep
+}
+
+// LookupDockerID looks up endpoint by Docker ID
+func LookupDockerID(id string) *endpoint.Endpoint {
+	mutex.RLock()
+	ep := lookupDockerID(id)
+	mutex.RUnlock()
+	return ep
+}
+
+// LookupIPv4 looks up endpoint by IPv4 address
+func LookupIPv4(ipv4 string) *endpoint.Endpoint {
+	mutex.RLock()
+	ep := lookupIPv4(ipv4)
+	mutex.RUnlock()
+	return ep
+}
+
+// LookupPodName looks up endpoint by namespace + pod name
+func LookupPodName(name string) *endpoint.Endpoint {
+	mutex.RLock()
+	ep := lookupPodNameLocked(name)
+	mutex.RUnlock()
+	return ep
+}
+
+// UpdateReferences makes an endpoint available by all possible reference
+// fields as available for this endpoint (containerID, IPv4 address, ...)
+// Must be called with ep.Mutex.RLock held.
+func UpdateReferences(ep *endpoint.Endpoint) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	updateReferences(ep)
+}
+
+// Remove removes the endpoint from the global maps.
+// Must be called with ep.Mutex.RLock held.
+func Remove(ep *endpoint.Endpoint) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	delete(endpoints, ep.ID)
+
+	if ep.ContainerID != "" {
+		delete(endpointsAux, endpointid.NewID(endpointid.ContainerIdPrefix, ep.ContainerID))
+	}
+
+	if ep.DockerEndpointID != "" {
+		delete(endpointsAux, endpointid.NewID(endpointid.DockerEndpointPrefix, ep.DockerEndpointID))
+	}
+
+	if ep.IPv4.String() != "" {
+		delete(endpointsAux, endpointid.NewID(endpointid.IPv4Prefix, ep.IPv4.String()))
+	}
+
+	if ep.ContainerName != "" {
+		delete(endpointsAux, endpointid.NewID(endpointid.ContainerNamePrefix, ep.ContainerName))
+	}
+
+	if podName := ep.GetK8sNamespaceAndPodNameLocked(); podName != "" {
+		delete(endpointsAux, endpointid.NewID(endpointid.PodNamePrefix, podName))
+	}
+}
+
+// RemoveAll removes all endpoints from the global maps.
+func RemoveAll() {
+	mutex.Lock()
+	defer mutex.Unlock()
+	endpoints = map[uint16]*endpoint.Endpoint{}
+	endpointsAux = map[string]*endpoint.Endpoint{}
+}
+
+// lookupCiliumID looks up endpoint by endpoint ID
+func lookupCiliumID(id uint16) *endpoint.Endpoint {
+	if ep, ok := endpoints[id]; ok {
+		return ep
+	}
+	return nil
+}
+
+func lookupDockerEndpoint(id string) *endpoint.Endpoint {
+	if ep, ok := endpointsAux[endpointid.NewID(endpointid.DockerEndpointPrefix, id)]; ok {
+		return ep
+	}
+	return nil
+}
+
+func lookupPodNameLocked(name string) *endpoint.Endpoint {
+	if ep, ok := endpointsAux[endpointid.NewID(endpointid.PodNamePrefix, name)]; ok {
+		return ep
+	}
+	return nil
+}
+
+func lookupDockerContainerName(name string) *endpoint.Endpoint {
+	if ep, ok := endpointsAux[endpointid.NewID(endpointid.ContainerNamePrefix, name)]; ok {
+		return ep
+	}
+	return nil
+}
+
+func lookupIPv4(ipv4 string) *endpoint.Endpoint {
+	if ep, ok := endpointsAux[endpointid.NewID(endpointid.IPv4Prefix, ipv4)]; ok {
+		return ep
+	}
+	return nil
+}
+
+func lookupDockerID(id string) *endpoint.Endpoint {
+	if ep, ok := endpointsAux[endpointid.NewID(endpointid.ContainerIdPrefix, id)]; ok {
+		return ep
+	}
+	return nil
+}
+
+func linkContainerID(ep *endpoint.Endpoint) {
+	endpointsAux[endpointid.NewID(endpointid.ContainerIdPrefix, ep.ContainerID)] = ep
+}
+
+// UpdateReferences updates the mappings of various values to their corresponding
+// endpoints, such as ContainerID, Docker Container Name, Pod Name, etc.
+func updateReferences(ep *endpoint.Endpoint) {
+	if ep.ContainerID != "" {
+		linkContainerID(ep)
+	}
+
+	if ep.DockerEndpointID != "" {
+		endpointsAux[endpointid.NewID(endpointid.DockerEndpointPrefix, ep.DockerEndpointID)] = ep
+	}
+
+	if ep.IPv4.String() != "" {
+		endpointsAux[endpointid.NewID(endpointid.IPv4Prefix, ep.IPv4.String())] = ep
+	}
+
+	if ep.ContainerName != "" {
+		endpointsAux[endpointid.NewID(endpointid.ContainerNamePrefix, ep.ContainerName)] = ep
+	}
+
+	if podName := ep.GetK8sNamespaceAndPodNameLocked(); podName != "" {
+		endpointsAux[endpointid.NewID(endpointid.PodNamePrefix, podName)] = ep
+	}
+}
+
+// RegenerateAllEndpoints calls a SetStateLocked for each endpoint and
+// regenerates if state transaction is valid. During this process, the endpoint
+// list is locked and cannot be modified.
+// Returns a waiting group that can be used to know when all the endpoints are
+// regenerated.
+func RegenerateAllEndpoints(owner endpoint.Owner, regenContext *endpoint.RegenerationContext) *sync.WaitGroup {
+	var wg sync.WaitGroup
+
+	eps := GetEndpoints()
+	wg.Add(len(eps))
+
+	log.Infof("regenerating all endpoints due to %s", regenContext.Reason)
+	for _, ep := range eps {
+		go func(ep *endpoint.Endpoint, wg *sync.WaitGroup) {
+			if err := ep.LockAlive(); err != nil {
+				log.WithError(err).Warn("Error regenerating endpoint for event %s", regenContext.Reason)
+				ep.LogStatus(endpoint.Policy, endpoint.Failure, "Error while handling policy updates for endpoint: "+err.Error())
+			} else {
+				regen := ep.SetStateLocked(endpoint.StateWaitingToRegenerate, fmt.Sprintf("Triggering endpoint regeneration due to %s", regenContext.Reason))
+				ep.Unlock()
+				if regen {
+					// Regenerate logs status according to the build success/failure
+					<-ep.Regenerate(owner, regenContext)
+				}
+			}
+			wg.Done()
+		}(ep, &wg)
+	}
+
+	return &wg
+}
+
+// HasGlobalCT returns true if the endpoints have a global CT, false otherwise.
+func HasGlobalCT() bool {
+	eps := GetEndpoints()
+	for _, e := range eps {
+		if !e.Options.IsEnabled(option.ConntrackLocal) {
+			return true
+		}
+	}
+	return false
+}
+
+// GetEndpoints returns a slice of all endpoints present in endpoint manager.
+func GetEndpoints() []*endpoint.Endpoint {
+	mutex.RLock()
+	eps := make([]*endpoint.Endpoint, 0, len(endpoints))
+	for _, ep := range endpoints {
+		eps = append(eps, ep)
+	}
+	mutex.RUnlock()
+	return eps
+}
+
+// AddEndpoint takes the prepared endpoint object and starts managing it.
+func AddEndpoint(owner endpoint.Owner, ep *endpoint.Endpoint, reason string) (err error) {
+	alwaysEnforce := policy.GetPolicyEnabled() == option.AlwaysEnforce
+	ep.SetIngressPolicyEnabled(alwaysEnforce)
+	ep.SetEgressPolicyEnabled(alwaysEnforce)
+
+	if err := ep.CreateDirectory(); err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			ep.RemoveDirectory()
+		}
+	}()
+
+	// Regenerate immediately if ready or waiting for identity
+	if err := ep.LockAlive(); err != nil {
+		return err
+	}
+	build := false
+	state := ep.GetStateLocked()
+
+	// We can only trigger regeneration of endpoints if the endpoint is in a
+	// state where it can regenerate. See endpoint.SetStateLocked().
+	if state == endpoint.StateReady {
+		ep.SetStateLocked(endpoint.StateWaitingToRegenerate, reason)
+		build = true
+	}
+	ep.Unlock()
+
+	if build {
+		if err := ep.RegenerateWait(owner, reason); err != nil {
+			return err
+		}
+	}
+
+	Insert(ep)
+	ep.InsertEvent()
+
+	return nil
+}
+
+// WaitForEndpointsAtPolicyRev waits for all endpoints which existed at the time
+// this function is called to be at a given policy revision.
+// New endpoints appearing while waiting are ignored.
+func WaitForEndpointsAtPolicyRev(ctx context.Context, rev uint64) error {
+	eps := GetEndpoints()
+	for i := range eps {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-eps[i].WaitForPolicyRevision(ctx, rev):
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
+		}
+	}
+	return nil
+}
---
+++ pkg/k8s/factory_functions.go
@@ -0,0 +1,505 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8s
+
+import (
+	"reflect"
+
+	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	versionedClient "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
+	"github.com/cilium/cilium/pkg/k8s/utils"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/versioned"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/api/extensions/v1beta1"
+	networkingv1 "k8s.io/api/networking/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func init() {
+	utils.RegisterObject(
+		&networkingv1.NetworkPolicy{},
+		"networkpolicies",
+		copyObjToV1NetworkPolicy,
+		listV1NetworkPolicies,
+		equalV1NetworkPolicy,
+	)
+
+	utils.RegisterObject(
+		&v1.Service{},
+		"services",
+		copyObjToV1Services,
+		listV1Services,
+		equalV1Services,
+	)
+
+	utils.RegisterObject(
+		&v1.Endpoints{},
+		"endpoints",
+		copyObjToV1Endpoints,
+		listV1Endpoints,
+		equalV1Endpoints,
+	)
+
+	utils.RegisterObject(
+		&v1beta1.Ingress{},
+		"ingresses",
+		copyObjToV1beta1Ingress,
+		listV1beta1Ingress,
+		equalV1beta1Ingress,
+	)
+
+	utils.RegisterObject(
+		&cilium_v2.CiliumNetworkPolicy{},
+		"ciliumnetworkpolicies",
+		copyObjToV2CNP,
+		listV2CNP,
+		equalV2CNP,
+	)
+
+	utils.RegisterObject(
+		&v1.Pod{},
+		"pods",
+		copyObjToV1Pod,
+		listV1Pod,
+		equalV1Pod,
+	)
+
+	utils.RegisterObject(
+		&v1.Node{},
+		"nodes",
+		copyObjToV1Node,
+		listV1Node,
+		equalV1Node,
+	)
+
+	utils.RegisterObject(
+		&v1.Namespace{},
+		"namespaces",
+		copyObjToV1Namespace,
+		listV1Namespace,
+		equalV1Namespace,
+	)
+}
+
+func copyObjToV1NetworkPolicy(obj interface{}) meta_v1.Object {
+	k8sNP, ok := obj.(*networkingv1.NetworkPolicy)
+	if !ok {
+		log.WithField(logfields.Object, logfields.Repr(obj)).
+			Warn("Ignoring invalid k8s v1 NetworkPolicy")
+		return nil
+	}
+	return k8sNP.DeepCopy()
+}
+
+func copyObjToV1Services(obj interface{}) meta_v1.Object {
+	svc, ok := obj.(*v1.Service)
+	if !ok {
+		log.WithField(logfields.Object, logfields.Repr(obj)).
+			Warn("Ignoring invalid k8s v1 Service")
+		return nil
+	}
+	return svc.DeepCopy()
+}
+
+func copyObjToV1Endpoints(obj interface{}) meta_v1.Object {
+	ep, ok := obj.(*v1.Endpoints)
+	if !ok {
+		log.WithField(logfields.Object, logfields.Repr(obj)).
+			Warn("Ignoring invalid k8s v1 Endpoints")
+		return nil
+	}
+	return ep.DeepCopy()
+}
+
+func copyObjToV1beta1Ingress(obj interface{}) meta_v1.Object {
+	ing, ok := obj.(*v1beta1.Ingress)
+	if !ok {
+		log.WithField(logfields.Object, logfields.Repr(obj)).
+			Warn("Ignoring invalid k8s v1beta1 Ingress")
+		return nil
+	}
+	return ing.DeepCopy()
+}
+
+func copyObjToV2CNP(obj interface{}) meta_v1.Object {
+	cnp, ok := obj.(*cilium_v2.CiliumNetworkPolicy)
+	if !ok {
+		log.WithField(logfields.Object, logfields.Repr(obj)).
+			Warn("Ignoring invalid k8s v2 CiliumNetworkPolicy")
+		return nil
+	}
+	return cnp.DeepCopy()
+}
+
+func copyObjToV1Pod(obj interface{}) meta_v1.Object {
+	pod, ok := obj.(*v1.Pod)
+	if !ok {
+		log.WithField(logfields.Object, logfields.Repr(obj)).
+			Warn("Ignoring invalid k8s v1 Pod")
+		return nil
+	}
+	return pod.DeepCopy()
+}
+
+func copyObjToV1Node(obj interface{}) meta_v1.Object {
+	node, ok := obj.(*v1.Node)
+	if !ok {
+		log.WithField(logfields.Object, logfields.Repr(obj)).
+			Warn("Ignoring invalid k8s v1 Node")
+		return nil
+	}
+	return node.DeepCopy()
+}
+
+func copyObjToV1Namespace(obj interface{}) meta_v1.Object {
+	ns, ok := obj.(*v1.Namespace)
+	if !ok {
+		log.WithField(logfields.Object, logfields.Repr(obj)).
+			Warn("Ignoring invalid k8s v1 Namespace")
+		return nil
+	}
+	return ns.DeepCopy()
+}
+
+func listV1NetworkPolicies(client interface{}) func() (versioned.Map, error) {
+	k8sClient, ok := client.(kubernetes.Interface)
+	if !ok {
+		log.Panicf("Invalid resource type %s: expecting 'kubernetes.Interface'", reflect.TypeOf(client))
+	}
+	return func() (versioned.Map, error) {
+		m := versioned.NewMap()
+		// Limit the number of elements to avoid network congestion every N minutes
+		lo := meta_v1.ListOptions{Limit: 50}
+		for {
+			list, err := k8sClient.NetworkingV1().NetworkPolicies("").List(lo)
+			if err != nil {
+				return nil, err
+			}
+			lo.Continue = list.Continue
+			for i := range list.Items {
+				m.Add(utils.GetVerStructFrom(&list.Items[i]))
+			}
+			if lo.Continue == "" {
+				break
+			}
+		}
+		return m, nil
+	}
+}
+
+func listV1Services(client interface{}) func() (versioned.Map, error) {
+	k8sClient, ok := client.(kubernetes.Interface)
+	if !ok {
+		log.Panicf("Invalid resource type %s: expecting 'kubernetes.Interface'", reflect.TypeOf(client))
+	}
+	return func() (versioned.Map, error) {
+		m := versioned.NewMap()
+		// Limit the number of elements to avoid network congestion every N minutes
+		lo := meta_v1.ListOptions{Limit: 50}
+		for {
+			list, err := k8sClient.CoreV1().Services("").List(lo)
+			if err != nil {
+				return nil, err
+			}
+			lo.Continue = list.Continue
+			for i := range list.Items {
+				m.Add(utils.GetVerStructFrom(&list.Items[i]))
+			}
+			if lo.Continue == "" {
+				break
+			}
+		}
+		return m, nil
+	}
+}
+
+func listV1Endpoints(client interface{}) func() (versioned.Map, error) {
+	k8sClient, ok := client.(kubernetes.Interface)
+	if !ok {
+		log.Panicf("Invalid resource type %s: expecting 'kubernetes.Interface'", reflect.TypeOf(client))
+	}
+	return func() (versioned.Map, error) {
+		m := versioned.NewMap()
+		// Limit the number of elements to avoid network congestion every N minutes
+		lo := meta_v1.ListOptions{Limit: 50}
+		for {
+			list, err := k8sClient.CoreV1().Endpoints("").List(lo)
+			if err != nil {
+				return nil, err
+			}
+			lo.Continue = list.Continue
+			for i := range list.Items {
+				m.Add(utils.GetVerStructFrom(&list.Items[i]))
+			}
+			if lo.Continue == "" {
+				break
+			}
+		}
+		return m, nil
+	}
+}
+
+func listV1beta1Ingress(client interface{}) func() (versioned.Map, error) {
+	k8sClient, ok := client.(kubernetes.Interface)
+	if !ok {
+		log.Panicf("Invalid resource type %s: expecting 'kubernetes.Interface'", reflect.TypeOf(client))
+	}
+	return func() (versioned.Map, error) {
+		m := versioned.NewMap()
+		// Limit the number of elements to avoid network congestion every N minutes
+		lo := meta_v1.ListOptions{Limit: 50}
+		for {
+			list, err := k8sClient.ExtensionsV1beta1().Ingresses("").List(lo)
+			if err != nil {
+				return nil, err
+			}
+			lo.Continue = list.Continue
+			for i := range list.Items {
+				m.Add(utils.GetVerStructFrom(&list.Items[i]))
+			}
+			if lo.Continue == "" {
+				break
+			}
+		}
+		return m, nil
+	}
+}
+
+func listV2CNP(client interface{}) func() (versioned.Map, error) {
+	k8sClient, ok := client.(versionedClient.Interface)
+	if !ok {
+		log.Panicf("Invalid resource type %s: expecting 'versionedClient.Interface'", reflect.TypeOf(client))
+	}
+	return func() (versioned.Map, error) {
+		m := versioned.NewMap()
+		// Limit the number of elements to avoid network congestion every N minutes
+		lo := meta_v1.ListOptions{Limit: 50}
+		for {
+			list, err := k8sClient.CiliumV2().CiliumNetworkPolicies("").List(lo)
+			if err != nil {
+				return nil, err
+			}
+			lo.Continue = list.Continue
+			for i := range list.Items {
+				m.Add(utils.GetVerStructFrom(&list.Items[i]))
+			}
+			if lo.Continue == "" {
+				break
+			}
+		}
+		return m, nil
+	}
+}
+
+func listV1Pod(client interface{}) func() (versioned.Map, error) {
+	k8sClient, ok := client.(kubernetes.Interface)
+	if !ok {
+		log.Panicf("Invalid resource type %s: expecting 'kubernetes.Interface'", reflect.TypeOf(client))
+	}
+	return func() (versioned.Map, error) {
+		m := versioned.NewMap()
+		// Limit the number of elements to avoid network congestion every N minutes
+		lo := meta_v1.ListOptions{Limit: 50}
+		for {
+			list, err := k8sClient.CoreV1().Pods("").List(lo)
+			if err != nil {
+				return nil, err
+			}
+			lo.Continue = list.Continue
+			for i := range list.Items {
+				m.Add(utils.GetVerStructFrom(&list.Items[i]))
+			}
+			if lo.Continue == "" {
+				break
+			}
+		}
+		return m, nil
+	}
+}
+
+func listV1Node(client interface{}) func() (versioned.Map, error) {
+	k8sClient, ok := client.(kubernetes.Interface)
+	if !ok {
+		log.Panicf("Invalid resource type %s: expecting 'kubernetes.Interface'", reflect.TypeOf(client))
+	}
+	return func() (versioned.Map, error) {
+		m := versioned.NewMap()
+		// Limit the number of elements to avoid network congestion every N minutes
+		lo := meta_v1.ListOptions{Limit: 50}
+		for {
+			list, err := k8sClient.CoreV1().Nodes().List(lo)
+			if err != nil {
+				return nil, err
+			}
+			lo.Continue = list.Continue
+			for i := range list.Items {
+				m.Add(utils.GetVerStructFrom(&list.Items[i]))
+			}
+			if lo.Continue == "" {
+				break
+			}
+		}
+		return m, nil
+	}
+}
+
+func listV1Namespace(client interface{}) func() (versioned.Map, error) {
+	k8sClient, ok := client.(kubernetes.Interface)
+	if !ok {
+		log.Panicf("Invalid resource type %s: expecting 'kubernetes.Interface'", reflect.TypeOf(client))
+	}
+	return func() (versioned.Map, error) {
+		m := versioned.NewMap()
+		// Limit the number of elements to avoid network congestion every N minutes
+		lo := meta_v1.ListOptions{Limit: 50}
+		for {
+			list, err := k8sClient.CoreV1().Namespaces().List(lo)
+			if err != nil {
+				return nil, err
+			}
+			lo.Continue = list.Continue
+			for i := range list.Items {
+				m.Add(utils.GetVerStructFrom(&list.Items[i]))
+			}
+			if lo.Continue == "" {
+				break
+			}
+		}
+		return m, nil
+	}
+}
+
+func equalV1NetworkPolicy(o1, o2 interface{}) bool {
+	_, ok := o1.(*networkingv1.NetworkPolicy)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *networkingv1.NetworkPolicy", reflect.TypeOf(o1))
+		return false
+	}
+	_, ok = o1.(*networkingv1.NetworkPolicy)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *networkingv1.NetworkPolicy", reflect.TypeOf(o2))
+		return false
+	}
+	// FIXME write dedicated deep equal function
+	return false
+}
+
+func equalV1Services(o1, o2 interface{}) bool {
+	_, ok := o1.(*v1.Service)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Service", reflect.TypeOf(o1))
+		return false
+	}
+	_, ok = o1.(*v1.Service)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Service", reflect.TypeOf(o2))
+		return false
+	}
+	// FIXME write dedicated deep equal function
+	return false
+}
+
+func equalV1Endpoints(o1, o2 interface{}) bool {
+	_, ok := o1.(*v1.Endpoints)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Endpoints", reflect.TypeOf(o1))
+		return false
+	}
+	_, ok = o1.(*v1.Endpoints)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Endpoints", reflect.TypeOf(o2))
+		return false
+	}
+	// FIXME write dedicated deep equal function
+	return false
+}
+
+func equalV1beta1Ingress(o1, o2 interface{}) bool {
+	_, ok := o1.(*v1beta1.Ingress)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1beta1.Ingress", reflect.TypeOf(o1))
+		return false
+	}
+	_, ok = o1.(*v1beta1.Ingress)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1beta1.Ingress", reflect.TypeOf(o2))
+		return false
+	}
+	// FIXME write dedicated deep equal function
+	return false
+}
+
+func equalV2CNP(o1, o2 interface{}) bool {
+	_, ok := o1.(*cilium_v2.CiliumNetworkPolicy)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *cilium_v2.CiliumNetworkPolicy", reflect.TypeOf(o1))
+		return false
+	}
+	_, ok = o1.(*cilium_v2.CiliumNetworkPolicy)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *cilium_v2.CiliumNetworkPolicy", reflect.TypeOf(o2))
+		return false
+	}
+	// FIXME write dedicated deep equal function
+	return false
+}
+
+func equalV1Pod(o1, o2 interface{}) bool {
+	_, ok := o1.(*v1.Pod)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Pod", reflect.TypeOf(o1))
+		return false
+	}
+	_, ok = o1.(*v1.Pod)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Pod", reflect.TypeOf(o2))
+		return false
+	}
+	// FIXME write dedicated deep equal function
+	return false
+}
+
+func equalV1Node(o1, o2 interface{}) bool {
+	_, ok := o1.(*v1.Node)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Node", reflect.TypeOf(o1))
+		return false
+	}
+	_, ok = o1.(*v1.Node)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Node", reflect.TypeOf(o2))
+		return false
+	}
+	// FIXME write dedicated deep equal function
+	return false
+}
+
+func equalV1Namespace(o1, o2 interface{}) bool {
+	_, ok := o1.(*v1.Namespace)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Namespace", reflect.TypeOf(o1))
+		return false
+	}
+	_, ok = o1.(*v1.Namespace)
+	if !ok {
+		log.Panicf("Invalid resource type %q, expecting *v1.Namespace", reflect.TypeOf(o2))
+		return false
+	}
+	// FIXME write dedicated deep equal function
+	return false
+}
