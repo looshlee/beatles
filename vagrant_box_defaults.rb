364c6273386b5c8706c13e8edf7b609e8f9cfa2d
elle c <14810964+looshlee@users.noreply.github.com>
2022-07-27 10:58:53 -0700

    Initialize pr/v1.7-backport-2020-12-03

---
+++ vagrant_box_defaults.rb
@@ -0,0 +1,10 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby
+Vagrant.require_version ">= 2.2.0"
+
+$SERVER_BOX = "cilium/ubuntu-dev"
+$SERVER_VERSION= "166"
+$NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
+$NETNEXT_SERVER_VERSION= "44"
+@v419_SERVER_BOX= "cilium/ubuntu-4-19"
+@v419_SERVER_VERSION= "11"
