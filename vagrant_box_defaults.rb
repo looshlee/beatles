a3e294015ebd515e3ffa5d040f7be457f98631f6
elle c <14810964+looshlee@users.noreply.github.com>
2022-07-27 11:05:15 -0700

    Initialize pr/rolinh/hubble-proto-gen-go-grpc-v1.0.0

---
+++ vagrant_box_defaults.rb
@@ -0,0 +1,12 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby
+Vagrant.require_version ">= 2.2.0"
+
+$SERVER_BOX = "cilium/ubuntu-dev"
+$SERVER_VERSION= "192"
+$NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
+$NETNEXT_SERVER_VERSION= "86"
+@v419_SERVER_BOX= "cilium/ubuntu-4-19"
+@v419_SERVER_VERSION= "33"
+@v49_SERVER_BOX= "cilium/ubuntu"
+@v49_SERVER_VERSION= "192"
