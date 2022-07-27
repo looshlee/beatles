000fde4aca729116b4c63f0a58dc21b2cef0bf0a
elle c <14810964+looshlee@users.noreply.github.com>
2022-07-27 11:04:45 -0700

    Initialize hf/master/hf-master

---
+++ pkg/hubble/container/ring_reader_test.go
@@ -0,0 +1,286 @@
+// Copyright 2020 Authors of Hubble
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package container
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"testing"
+	"time"
+
+	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/goleak"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func TestRingReader_Previous(t *testing.T) {
+	ring := NewRing(Capacity15)
+	for i := 0; i < 15; i++ {
+		ring.Write(&v1.Event{Timestamp: &timestamppb.Timestamp{Seconds: int64(i)}})
+	}
+	tests := []struct {
+		start   uint64
+		count   int
+		want    []*v1.Event
+		wantErr error
+	}{
+		{
+			start: 13,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 13}},
+			},
+		}, {
+			start: 13,
+			count: 2,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 13}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 12}},
+			},
+		}, {
+			start: 5,
+			count: 5,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 5}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 4}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 3}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 2}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 1}},
+			},
+		}, {
+			start: 0,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 0}},
+			},
+		}, {
+			start: 0,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 0}},
+			},
+		}, {
+			start:   14,
+			count:   1,
+			wantErr: io.EOF,
+		},
+		{
+			start:   ^uint64(0),
+			count:   1,
+			wantErr: ErrInvalidRead,
+		},
+	}
+	for _, tt := range tests {
+		name := fmt.Sprintf("read %d, start at position %d", tt.count, tt.start)
+		t.Run(name, func(t *testing.T) {
+			reader := NewRingReader(ring, tt.start)
+			var got []*v1.Event
+			for i := 0; i < tt.count; i++ {
+				event, err := reader.Previous()
+				if err != tt.wantErr {
+					t.Errorf(`"%s" error = %v, wantErr %v`, name, err, tt.wantErr)
+				}
+				if err != nil {
+					return
+				}
+				got = append(got, event)
+			}
+			assert.Equal(t, tt.want, got)
+			assert.Nil(t, reader.Close())
+		})
+	}
+}
+
+func TestRingReader_Next(t *testing.T) {
+	ring := NewRing(Capacity15)
+	for i := 0; i < 15; i++ {
+		ring.Write(&v1.Event{Timestamp: &timestamppb.Timestamp{Seconds: int64(i)}})
+	}
+
+	tests := []struct {
+		start   uint64
+		count   int
+		want    []*v1.Event
+		wantErr error
+	}{
+		{
+			start: 0,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 0}},
+			},
+		}, {
+			start: 0,
+			count: 2,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 0}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 1}},
+			},
+		}, {
+			start: 5,
+			count: 5,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 5}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 6}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 7}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 8}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 9}},
+			},
+		}, {
+			start: 13,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 13}},
+			},
+		}, {
+			start:   ^uint64(0),
+			count:   1,
+			wantErr: ErrInvalidRead,
+		}, {
+			start:   14,
+			count:   1,
+			wantErr: io.EOF,
+		},
+	}
+	for _, tt := range tests {
+		name := fmt.Sprintf("read %d, start at position %d", tt.count, tt.start)
+		t.Run(name, func(t *testing.T) {
+			reader := NewRingReader(ring, tt.start)
+			var got []*v1.Event
+			for i := 0; i < tt.count; i++ {
+				event, err := reader.Next()
+				if err != tt.wantErr {
+					t.Errorf(`"%s" error = %v, wantErr %v`, name, err, tt.wantErr)
+				}
+				if err != nil {
+					return
+				}
+				got = append(got, event)
+			}
+			assert.Equal(t, tt.want, got)
+			assert.Nil(t, reader.Close())
+		})
+	}
+}
+
+func TestRingReader_NextFollow(t *testing.T) {
+	defer goleak.VerifyNone(
+		t,
+		// ignore go routines started by the redirect we do from klog to logrus
+		goleak.IgnoreTopFunction("k8s.io/klog.(*loggingT).flushDaemon"),
+		goleak.IgnoreTopFunction("k8s.io/klog/v2.(*loggingT).flushDaemon"),
+		goleak.IgnoreTopFunction("io.(*pipe).Read"))
+	ring := NewRing(Capacity15)
+	for i := 0; i < 15; i++ {
+		ring.Write(&v1.Event{Timestamp: &timestamppb.Timestamp{Seconds: int64(i)}})
+	}
+
+	tests := []struct {
+		start       uint64
+		count       int
+		want        []*v1.Event
+		wantTimeout bool
+	}{
+		{
+			start: 0,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 0}},
+			},
+		}, {
+			start: 0,
+			count: 2,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 0}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 1}},
+			},
+		}, {
+			start: 5,
+			count: 5,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 5}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 6}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 7}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 8}},
+				{Timestamp: &timestamppb.Timestamp{Seconds: 9}},
+			},
+		}, {
+			start: 13,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &timestamppb.Timestamp{Seconds: 13}},
+			},
+		}, {
+			start:       14,
+			count:       1,
+			want:        []*v1.Event{nil},
+			wantTimeout: true,
+		},
+	}
+	for _, tt := range tests {
+		name := fmt.Sprintf("read %d, start at position %d, expect timeout=%t", tt.count, tt.start, tt.wantTimeout)
+		t.Run(name, func(t *testing.T) {
+			reader := NewRingReader(ring, tt.start)
+			var timedOut bool
+			var got []*v1.Event
+			for i := 0; i < tt.count; i++ {
+				ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+				got = append(got, reader.NextFollow(ctx))
+				select {
+				case <-ctx.Done():
+					timedOut = true
+				default:
+					assert.NotNil(t, got[i])
+				}
+				cancel()
+				assert.Nil(t, reader.Close())
+			}
+			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.wantTimeout, timedOut)
+		})
+	}
+}
+
+func TestRingReader_NextFollow_WithEmptyRing(t *testing.T) {
+	defer goleak.VerifyNone(
+		t,
+		// ignore go routines started by the redirect we do from klog to logrus
+		goleak.IgnoreTopFunction("k8s.io/klog.(*loggingT).flushDaemon"),
+		goleak.IgnoreTopFunction("k8s.io/klog/v2.(*loggingT).flushDaemon"),
+		goleak.IgnoreTopFunction("io.(*pipe).Read"))
+	ring := NewRing(Capacity15)
+	reader := NewRingReader(ring, ring.LastWriteParallel())
+	ctx, cancel := context.WithCancel(context.Background())
+	c := make(chan *v1.Event)
+	go func() {
+		select {
+		case <-ctx.Done():
+		case c <- reader.NextFollow(ctx):
+		}
+	}()
+	select {
+	case <-c:
+		t.Fail()
+	case <-time.After(100 * time.Millisecond):
+		// the call blocked, we're good
+	}
+	cancel()
+	assert.Nil(t, reader.Close())
+}
---
+++ pkg/k8s/apis/cilium.io/v2/ccnp_types.go
@@ -0,0 +1,156 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v2
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+
+	k8sCiliumUtils "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/utils"
+	"github.com/cilium/cilium/pkg/policy/api"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +deepequal-gen:private-method=true
+// +kubebuilder:resource:singular="ciliumclusterwidenetworkpolicy",path="ciliumclusterwidenetworkpolicies",scope="Cluster",shortName={ccnp}
+// +kubebuilder:subresource:status
+// +kubebuilder:storageversion
+
+// CiliumClusterwideNetworkPolicy is a Kubernetes third-party resource with an
+// modified version of CiliumNetworkPolicy which is cluster scoped rather than
+// namespace scoped.
+type CiliumClusterwideNetworkPolicy struct {
+	// +deepequal-gen=false
+	metav1.TypeMeta `json:",inline"`
+	// +deepequal-gen=false
+	metav1.ObjectMeta `json:"metadata"`
+
+	// Spec is the desired Cilium specific rule specification.
+	Spec *api.Rule `json:"spec,omitempty"`
+
+	// Specs is a list of desired Cilium specific rule specification.
+	Specs api.Rules `json:"specs,omitempty"`
+
+	// Status is the status of the Cilium policy rule.
+	//
+	// The reason this field exists in this structure is due a bug in the k8s
+	// code-generator that doesn't create a `UpdateStatus` method because the
+	// field does not exist in the structure.
+	//
+	// +kubebuilder:validation:Optional
+	Status CiliumNetworkPolicyStatus `json:"status"`
+}
+
+// DeepEqual compares 2 CCNPs while ignoring the LastAppliedConfigAnnotation
+// and ignoring the Status field of the CCNP.
+func (in *CiliumClusterwideNetworkPolicy) DeepEqual(other *CiliumClusterwideNetworkPolicy) bool {
+	return objectMetaDeepEqual(in.ObjectMeta, other.ObjectMeta) && in.deepEqual(other)
+}
+
+// GetPolicyStatus returns the CiliumClusterwideNetworkPolicyNodeStatus corresponding to
+// nodeName in the provided CiliumClusterwideNetworkPolicy. If Nodes within the rule's
+// Status is nil, returns an empty CiliumClusterwideNetworkPolicyNodeStatus.
+func (r *CiliumClusterwideNetworkPolicy) GetPolicyStatus(nodeName string) CiliumNetworkPolicyNodeStatus {
+	if r.Status.Nodes == nil {
+		return CiliumNetworkPolicyNodeStatus{}
+	}
+	return r.Status.Nodes[nodeName]
+}
+
+// SetPolicyStatus sets the given policy status for the given nodes' map.
+func (r *CiliumClusterwideNetworkPolicy) SetPolicyStatus(nodeName string, cnpns CiliumNetworkPolicyNodeStatus) {
+	if r.Status.Nodes == nil {
+		r.Status.Nodes = map[string]CiliumNetworkPolicyNodeStatus{}
+	}
+	r.Status.Nodes[nodeName] = cnpns
+}
+
+// SetDerivedPolicyStatus set the derivative policy status for the given
+// derivative policy name.
+func (r *CiliumClusterwideNetworkPolicy) SetDerivedPolicyStatus(derivativePolicyName string, status CiliumNetworkPolicyNodeStatus) {
+	if r.Status.DerivativePolicies == nil {
+		r.Status.DerivativePolicies = map[string]CiliumNetworkPolicyNodeStatus{}
+	}
+	r.Status.DerivativePolicies[derivativePolicyName] = status
+}
+
+// AnnotationsEquals returns true if ObjectMeta.Annotations of each
+// CiliumClusterwideNetworkPolicy are equivalent (i.e., they contain equivalent key-value
+// pairs).
+func (r *CiliumClusterwideNetworkPolicy) AnnotationsEquals(o *CiliumClusterwideNetworkPolicy) bool {
+	if o == nil {
+		return r == nil
+	}
+	return reflect.DeepEqual(r.ObjectMeta.Annotations, o.ObjectMeta.Annotations)
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:openapi-gen=false
+// +deepequal-gen=false
+
+// CiliumClusterwideNetworkPolicyList is a list of
+// CiliumClusterwideNetworkPolicy objects.
+type CiliumClusterwideNetworkPolicyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	// Items is a list of CiliumClusterwideNetworkPolicies.
+	Items []CiliumClusterwideNetworkPolicy `json:"items"`
+}
+
+// Parse parses a CiliumClusterwideNetworkPolicy and returns a list of cilium
+// policy rules.
+func (r *CiliumClusterwideNetworkPolicy) Parse() (api.Rules, error) {
+	if r.ObjectMeta.Name == "" {
+		return nil, fmt.Errorf("CiliumClusterwideNetworkPolicy must have name")
+	}
+
+	name := r.ObjectMeta.Name
+	uid := r.ObjectMeta.UID
+
+	retRules := api.Rules{}
+
+	if r.Spec == nil && r.Specs == nil {
+		return nil, ErrEmptyCCNP
+	}
+
+	if r.Spec != nil {
+		if err := r.Spec.Sanitize(); err != nil {
+			return nil, fmt.Errorf("Invalid CiliumClusterwideNetworkPolicy spec: %s", err)
+		}
+		cr := k8sCiliumUtils.ParseToCiliumRule("", name, uid, r.Spec)
+		retRules = append(retRules, cr)
+	}
+	if r.Specs != nil {
+		for _, rule := range r.Specs {
+			if err := rule.Sanitize(); err != nil {
+				return nil, fmt.Errorf("Invalid CiliumClusterwideNetworkPolicy specs: %s", err)
+
+			}
+			cr := k8sCiliumUtils.ParseToCiliumRule("", name, uid, rule)
+			retRules = append(retRules, cr)
+		}
+	}
+
+	return retRules, nil
+}
+
+// ErrEmptyCCNP is an error representing a CCNP that is empty, which means it is
+// missing both a `spec` and `specs` (both are nil).
+var ErrEmptyCCNP = errors.New("Invalid CiliumClusterwideNetworkPolicy spec(s): empty policy")
