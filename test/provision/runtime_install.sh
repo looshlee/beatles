5598d990a30e076094932a7b1ea6d5e4b36d77eb
elle c <14810964+looshlee@users.noreply.github.com>
2022-07-27 11:06:25 -0700

    Initialize hf/v1.8/v1.8.6-eni-cidr-fix-1

---
+++ test/provision/runtime_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if ! [[ -z $DOCKER_LOGIN && -z $DOCKER_PASSWORD ]]; then
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_LOGIN}" --password-stdin
+fi
+
+HOST=$(hostname)
+PROVISIONSRC="/tmp/provision"
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "${PROVISIONSRC}/helpers.bash"
+
+sudo bash -c "echo MaxSessions 200 >> /etc/ssh/sshd_config"
+sudo systemctl restart ssh
+
+"${PROVISIONSRC}"/dns.sh
+"${PROVISIONSRC}"/compile.sh
+"${PROVISIONSRC}"/wait-cilium.sh
