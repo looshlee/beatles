715a2c691a66acd52a270e7da6e04f2d1b34a650
elle c <14810964+looshlee@users.noreply.github.com>
2022-07-27 10:37:14 -0700

    Initialize fix-dsbl-ipt-feeder-rules

---
+++ go.mod
@@ -0,0 +1,85 @@
+module github.com/cilium/cilium
+
+go 1.14
+
+// direct dependencies
+require (
+	github.com/Azure/azure-sdk-for-go v38.0.0+incompatible
+	github.com/Azure/go-autorest/autorest/azure/auth v0.4.2
+	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496
+	github.com/aws/aws-sdk-go-v2 v0.18.0
+	github.com/blang/semver v3.5.0+incompatible
+	github.com/c9s/goprocinfo v0.0.0-20190309065803-0b2ad9ac246b
+	github.com/cilium/arping v1.0.1-0.20190728065459-c5eaf8d7a710
+	github.com/cilium/ebpf v0.0.0-20191113100448-d9fb101ca1fb
+	github.com/cilium/hubble v0.5.1-0.20200331202457-bfc152cb7c8c
+	github.com/cilium/ipam v0.0.0-20200217195329-a46f8d55f9db
+	github.com/cilium/proxy v0.0.0-20200309181938-3cf80fe45d03
+	github.com/containernetworking/cni v0.7.1
+	github.com/containernetworking/plugins v0.8.2
+	github.com/davecgh/go-spew v1.1.1
+	github.com/docker/docker v0.7.3-0.20190327010347-be7ac8be2ae0
+	github.com/docker/libnetwork v0.0.0-20190128195551-d8d4c8cf03d7
+	github.com/go-openapi/errors v0.19.3
+	github.com/go-openapi/loads v0.19.5
+	github.com/go-openapi/runtime v0.19.4
+	github.com/go-openapi/spec v0.19.6
+	github.com/go-openapi/strfmt v0.19.4
+	github.com/go-openapi/swag v0.19.7
+	github.com/go-openapi/validate v0.19.5
+	github.com/gogo/protobuf v1.3.1
+	github.com/golang/protobuf v1.3.2
+	github.com/google/go-cmp v0.3.1
+	github.com/google/gopacket v1.1.17
+	github.com/google/gops v0.3.6
+	github.com/gorilla/mux v1.7.0
+	github.com/hashicorp/consul/api v1.2.0
+	github.com/hashicorp/go-immutable-radix v1.1.0
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/kevinburke/ssh_config v0.0.0-20190725054713-01f96b0aa0cd
+	github.com/kr/pretty v0.1.0
+	github.com/mattn/go-shellwords v1.0.5
+	github.com/miekg/dns v1.0.14
+	github.com/onsi/ginkgo v1.11.0
+	github.com/onsi/gomega v1.7.0
+	github.com/optiopay/kafka v0.0.0-00010101000000-000000000000
+	github.com/pborman/uuid v1.2.0
+	github.com/pkg/errors v0.8.1
+	github.com/pmezard/go-difflib v1.0.0
+	github.com/prometheus/client_golang v1.2.0
+	github.com/prometheus/client_model v0.2.0
+	github.com/russross/blackfriday v1.5.2
+	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa
+	github.com/servak/go-fastping v0.0.0-20160802140958-5718d12e20a0
+	github.com/shirou/gopsutil v0.0.0-20180427012116-c95755e4bcd7
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.6.1
+	github.com/vishvananda/netlink v1.1.1-0.20200210222539-bfba8e4149db
+	go.etcd.io/etcd v0.0.0-20191023171146-3cf2f69b5738
+	golang.org/x/crypto v0.0.0-20200220183623-bac4c82f6975
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55
+	google.golang.org/grpc v1.26.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127
+	gopkg.in/fsnotify.v1 v1.4.7
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.2.8
+	k8s.io/api v0.18.0
+	k8s.io/apiextensions-apiserver v0.18.0
+	k8s.io/apimachinery v0.18.0
+	k8s.io/client-go v0.18.0
+	k8s.io/code-generator v0.18.0
+	k8s.io/klog v1.0.0
+	sigs.k8s.io/yaml v1.2.0
+)
+
+replace (
+	github.com/miekg/dns => github.com/cilium/dns v1.1.4-0.20190417235132-8e25ec9a0ff3
+	github.com/optiopay/kafka => github.com/cilium/kafka v0.0.0-20180809090225-01ce283b732b
+	k8s.io/client-go => github.com/cilium/client-go v0.0.0-20200326103132-fe7bd31c2794
+)
---
+++ pkg/service/service_test.go
@@ -0,0 +1,258 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package service
+
+import (
+	"net"
+
+	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/service/healthserver"
+
+	"github.com/cilium/cilium/pkg/checker"
+	lb "github.com/cilium/cilium/pkg/loadbalancer"
+	"github.com/cilium/cilium/pkg/maps/lbmap"
+
+	. "gopkg.in/check.v1"
+)
+
+type ManagerTestSuite struct {
+	svc       *Service
+	lbmap     *lbmap.LBMockMap // for accessing public fields
+	svcHealth *healthserver.MockHealthHTTPServerFactory
+}
+
+var _ = Suite(&ManagerTestSuite{})
+
+func (m *ManagerTestSuite) SetUpTest(c *C) {
+	serviceIDAlloc.resetLocalID()
+	backendIDAlloc.resetLocalID()
+
+	m.svc = NewService(nil)
+	m.svc.lbmap = lbmap.NewLBMockMap()
+	m.lbmap = m.svc.lbmap.(*lbmap.LBMockMap)
+
+	m.svcHealth = healthserver.NewMockHealthHTTPServerFactory()
+	m.svc.healthServer = healthserver.WithHealthHTTPServerFactory(m.svcHealth)
+}
+
+func (e *ManagerTestSuite) TearDownTest(c *C) {
+	serviceIDAlloc.resetLocalID()
+	backendIDAlloc.resetLocalID()
+}
+
+var (
+	frontend1 = *lb.NewL3n4AddrID(lb.TCP, net.ParseIP("1.1.1.1"), 80, 0)
+	frontend2 = *lb.NewL3n4AddrID(lb.TCP, net.ParseIP("1.1.1.2"), 80, 0)
+	backends1 = []lb.Backend{
+		*lb.NewBackend(0, lb.TCP, net.ParseIP("10.0.0.1"), 8080),
+		*lb.NewBackend(0, lb.TCP, net.ParseIP("10.0.0.2"), 8080),
+	}
+	backends2 = []lb.Backend{
+		*lb.NewBackend(0, lb.TCP, net.ParseIP("10.0.0.2"), 8080),
+		*lb.NewBackend(0, lb.TCP, net.ParseIP("10.0.0.3"), 8080),
+	}
+)
+
+func (m *ManagerTestSuite) TestUpsertAndDeleteService(c *C) {
+	// Should create a new service with two backends
+	created, id1, err := m.svc.UpsertService(frontend1, backends1, lb.SVCTypeNodePort, lb.SVCTrafficPolicyCluster, 0, "svc1", "ns1")
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, true)
+	c.Assert(id1, Equals, lb.ID(1))
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].Backends), Equals, 2)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
+	c.Assert(m.svc.svcByID[id1].svcName, Equals, "svc1")
+	c.Assert(m.svc.svcByID[id1].svcNamespace, Equals, "ns1")
+
+	// Should update nothing
+	created, id1, err = m.svc.UpsertService(frontend1, backends1, lb.SVCTypeNodePort, lb.SVCTrafficPolicyCluster, 0, "", "")
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, false)
+	c.Assert(id1, Equals, lb.ID(1))
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].Backends), Equals, 2)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
+	c.Assert(m.svc.svcByID[id1].svcName, Equals, "svc1")
+	c.Assert(m.svc.svcByID[id1].svcNamespace, Equals, "ns1")
+	// TODO(brb) test that backends are the same
+	// TODO(brb) check that .backends =~ .backendsByHash
+
+	// Should remove one backend
+	created, id1, err = m.svc.UpsertService(frontend1, backends1[0:1], lb.SVCTypeNodePort, lb.SVCTrafficPolicyCluster, 0, "", "")
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, false)
+	c.Assert(id1, Equals, lb.ID(1))
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].Backends), Equals, 1)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 1)
+	c.Assert(m.svc.svcByID[id1].svcName, Equals, "svc1")
+	c.Assert(m.svc.svcByID[id1].svcNamespace, Equals, "ns1")
+
+	// Should add another service
+	created, id2, err := m.svc.UpsertService(frontend2, backends1, lb.SVCTypeNodePort, lb.SVCTrafficPolicyCluster, 0, "svc2", "ns2")
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, true)
+	c.Assert(id2, Equals, lb.ID(2))
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id2)].Backends), Equals, 2)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
+	c.Assert(m.svc.svcByID[id2].svcName, Equals, "svc2")
+	c.Assert(m.svc.svcByID[id2].svcNamespace, Equals, "ns2")
+
+	// Should remove the service and the backend, but keep another service and
+	// its backends
+	found, err := m.svc.DeleteServiceByID(lb.ServiceID(id1))
+	c.Assert(err, IsNil)
+	c.Assert(found, Equals, true)
+	c.Assert(len(m.lbmap.ServiceByID), Equals, 1)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
+
+	// Should delete both backends of service
+	created, id2, err = m.svc.UpsertService(frontend2, nil, lb.SVCTypeNodePort, lb.SVCTrafficPolicyCluster, 0, "", "")
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, false)
+	c.Assert(id2, Equals, lb.ID(2))
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id2)].Backends), Equals, 0)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
+	c.Assert(m.svc.svcByID[id2].svcName, Equals, "svc2")
+	c.Assert(m.svc.svcByID[id2].svcNamespace, Equals, "ns2")
+
+	// Should delete the remaining service
+	found, err = m.svc.DeleteServiceByID(lb.ServiceID(id2))
+	c.Assert(err, IsNil)
+	c.Assert(found, Equals, true)
+	c.Assert(len(m.lbmap.ServiceByID), Equals, 0)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
+}
+
+func (m *ManagerTestSuite) TestRestoreServices(c *C) {
+	_, id1, err := m.svc.UpsertService(frontend1, backends1, lb.SVCTypeNodePort, lb.SVCTrafficPolicyCluster, 0, "", "")
+	c.Assert(err, IsNil)
+	_, id2, err := m.svc.UpsertService(frontend2, backends2, lb.SVCTypeClusterIP, lb.SVCTrafficPolicyCluster, 0, "", "")
+	c.Assert(err, IsNil)
+
+	// Restart service, but keep the lbmap to restore services from
+	lbmap := m.svc.lbmap.(*lbmap.LBMockMap)
+	m.svc = NewService(nil)
+	m.svc.lbmap = lbmap
+	err = m.svc.RestoreServices()
+	c.Assert(err, IsNil)
+
+	// Backends have been restored
+	c.Assert(len(m.svc.backendByHash), Equals, 3)
+	backends := append(backends1, backends2...)
+	for _, b := range backends {
+		_, found := m.svc.backendByHash[b.Hash()]
+		c.Assert(found, Equals, true)
+	}
+
+	// Services have been restored too
+	c.Assert(len(m.svc.svcByID), Equals, 2)
+	c.Assert(m.svc.svcByID[id1].frontend, checker.DeepEquals, lbmap.ServiceByID[uint16(id1)].Frontend)
+	c.Assert(m.svc.svcByID[id1].backends, checker.DeepEquals, lbmap.ServiceByID[uint16(id1)].Backends)
+	c.Assert(m.svc.svcByID[id2].frontend, checker.DeepEquals, lbmap.ServiceByID[uint16(id2)].Frontend)
+	c.Assert(m.svc.svcByID[id2].backends, checker.DeepEquals, lbmap.ServiceByID[uint16(id2)].Backends)
+}
+
+func (m *ManagerTestSuite) TestSyncWithK8sFinished(c *C) {
+	_, _, err := m.svc.UpsertService(frontend1, backends1, lb.SVCTypeNodePort, lb.SVCTrafficPolicyCluster, 0, "", "")
+	c.Assert(err, IsNil)
+	_, _, err = m.svc.UpsertService(frontend2, backends2, lb.SVCTypeClusterIP, lb.SVCTrafficPolicyCluster, 0, "", "")
+	c.Assert(err, IsNil)
+	c.Assert(len(m.svc.svcByID), Equals, 2)
+
+	// Restart service, but keep the lbmap to restore services from
+	lbmap := m.svc.lbmap.(*lbmap.LBMockMap)
+	m.svc = NewService(nil)
+	m.svc.lbmap = lbmap
+	err = m.svc.RestoreServices()
+	c.Assert(err, IsNil)
+	c.Assert(len(m.svc.svcByID), Equals, 2)
+
+	// Imitate a situation where svc1 was deleted while we were down.
+	// In real life, the following upsert is called by k8s_watcher during
+	// the sync period of the cilium-agent's k8s service cache which happens
+	// during the initialization of cilium-agent.
+	_, id2, err := m.svc.UpsertService(frontend2, backends2, lb.SVCTypeClusterIP, lb.SVCTrafficPolicyCluster, 0, "svc2", "ns2")
+	c.Assert(err, IsNil)
+
+	// cilium-agent finished the initialization, and thus SyncWithK8sFinished
+	// is called
+	err = m.svc.SyncWithK8sFinished()
+	c.Assert(err, IsNil)
+
+	// svc1 should be removed from cilium
+	c.Assert(len(m.svc.svcByID), Equals, 1)
+	_, found := m.svc.svcByID[id2]
+	c.Assert(found, Equals, true)
+	c.Assert(m.svc.svcByID[id2].svcName, Equals, "svc2")
+	c.Assert(m.svc.svcByID[id2].svcNamespace, Equals, "ns2")
+}
+
+func (m *ManagerTestSuite) TestHealthCheckNodePort(c *C) {
+	// Create two node-local backends
+	be1 := *lb.NewBackend(0, lb.TCP, net.ParseIP("10.0.0.1"), 8080)
+	be2 := *lb.NewBackend(0, lb.TCP, net.ParseIP("10.0.0.2"), 8080)
+
+	// Insert svc1 with local backends
+	be2.NodeName = node.GetName()
+	be1.NodeName = node.GetName()
+	backends1 := []lb.Backend{be1, be2}
+
+	_, id1, err := m.svc.UpsertService(frontend1, backends1, lb.SVCTypeLoadBalancer, lb.SVCTrafficPolicyLocal, 32001, "svc1", "ns1")
+	c.Assert(err, IsNil)
+	c.Assert(m.svcHealth.ServiceByPort(32001).Service.Name, Equals, "svc1")
+	c.Assert(m.svcHealth.ServiceByPort(32001).Service.Namespace, Equals, "ns1")
+	c.Assert(m.svcHealth.ServiceByPort(32001).LocalEndpoints, Equals, len(backends1))
+
+	// Insert svc1 with remote backends
+	be1.NodeName = "remote-node"
+	be2.NodeName = "remote-node"
+	backends1 = []lb.Backend{be1, be2}
+	c.Assert(node.GetName(), Not(Equals), "remote-node")
+
+	new, _, err := m.svc.UpsertService(frontend1, backends1, lb.SVCTypeLoadBalancer, lb.SVCTrafficPolicyLocal, 32001, "svc1", "ns1")
+	c.Assert(err, IsNil)
+	c.Assert(new, Equals, false)
+	c.Assert(m.svcHealth.ServiceByPort(32001).Service.Name, Equals, "svc1")
+	c.Assert(m.svcHealth.ServiceByPort(32001).Service.Namespace, Equals, "ns1")
+	c.Assert(m.svcHealth.ServiceByPort(32001).LocalEndpoints, Equals, 0)
+
+	found, err := m.svc.DeleteServiceByID(lb.ServiceID(id1))
+	c.Assert(err, IsNil)
+	c.Assert(found, Equals, true)
+	c.Assert(m.svcHealth.ServiceByPort(32001), IsNil)
+}
+
+func (m *ManagerTestSuite) TestGetServiceNameByAddr(c *C) {
+	fe := frontend1.DeepCopy()
+	be := make([]lb.Backend, 0, len(backends1))
+	for _, backend := range backends1 {
+		be = append(be, *backend.DeepCopy())
+	}
+	name := "svc1"
+	namespace := "ns1"
+	hcport := uint16(3)
+	created, id1, err := m.svc.UpsertService(*fe, be, lb.SVCTypeNodePort, lb.SVCTrafficPolicyCluster, hcport, name, namespace)
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, true)
+	c.Assert(id1, Equals, lb.ID(1))
+	fe.ID = id1
+	gotNamespace, gotName, ok := m.svc.GetServiceNameByAddr(frontend1.L3n4Addr)
+	c.Assert(gotNamespace, Equals, namespace)
+	c.Assert(gotName, Equals, name)
+	c.Assert(ok, Equals, true)
+	_, _, ok = m.svc.GetServiceNameByAddr(frontend2.L3n4Addr)
+	c.Assert(ok, Equals, false)
+}
