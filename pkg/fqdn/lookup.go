c6d2b87e941184776ce14191c4e4a0889cbfa7cb
elle c <14810964+looshlee@users.noreply.github.com>
2022-07-27 10:51:20 -0700

    Initialize pr/nebril/test-rebase-helm

---
+++ pkg/fqdn/lookup.go
@@ -0,0 +1,30 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fqdn
+
+import (
+	"net"
+)
+
+// DNSIPRecords mimics the RR data from an A or AAAA response.
+// My kingdom for a DNS IP RR type that isn't hidden in the stdlib or has a
+// million layers of type indirection.
+type DNSIPRecords struct {
+	// TTL is the time, in seconds, that these IPs are valid for
+	TTL int
+
+	// IPs are the IPs associated with a DNS Name
+	IPs []net.IP
+}
